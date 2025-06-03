0ada9dd24e3a95e5a4523983b602015421bb3f2e
Guillermo <guillermo.cossio@imt-atlantique.fr>
2025-06-03 17:10:11 +0200

    Half-done pipeline
    dc3.yaml config file pretty much completed.
    Missing metrics and dataset setup.

--- dc3/evaluation/evaluation.py
+++ dc3/evaluation/evaluation.py
@@ -5,11 +5,18 @@
 
 from argparse import Namespace
 
-from dctools.data.dataloader import DatasetLoader
-from dctools.data.dataset import DCEmptyDataset
+import geopandas as gpd
+from loguru import logger
+import pandas as pd
+from shapely import geometry
+
+from dctools.data.datasets.dataset import get_dataset_from_config
+from dctools.data.datasets.dataloader import EvaluationDataloader
+from dctools.data.datasets.dataset_manager import MultiSourceDatasetManager
+
+from dctools.data.transforms import CustomTransforms
 from dctools.metrics.evaluator import Evaluator
 from dctools.metrics.metrics import MetricComputer
-from dctools.data.transforms import CustomTransforms
 from dctools.utilities.init_dask import setup_dask
 from dctools.utilities.xarray_utils import DICT_RENAME_CMEMS,\
     LIST_VARS_GLONET
@@ -26,74 +33,105 @@ class DC3Evaluation:
         """
         self.args = arguments
 
+    def setup_dataset_manager(self) -> MultiSourceDatasetManager:
+        # TODO
+
+        for source in self.args.sources:
+            source_name = source['dataset']
+            dataset = get_dataset_from_config(
+                    source,
+                    self.args.data_directory,
+                    self.args.catalog_dir,
+                    self.args.max_samples,
+                )
+        
+        match source_name:
+            case "modis":
+                modis_dataset = dataset
+            case "amsr2":
+                amsr2_dataset = dataset
+            case "iabp":
+                iabp_dataset = dataset
+        
+        """
+        Coordinate systems (CRS):
+
+        MODIS   EPSG:3413 (Arctic)  EPSG:3412 (Antarctic)   1.0km
+        AMSR2   EPSG:3413 (Arctic)  EPSG:3412 (Antarctic)   3.125km
+        """
+
+        """
+        x range from -3850 to 3750 (km)
+        y range from -5350 to 5850 (km)
+        """
+
+        filter_region = gpd.GeoSeries(geometry.Polygon((
+            (self.args.min_x,self.args.min_y),
+            (self.args.min_x,self.args.max_y),
+            (self.args.max_x,self.args.min_y),
+            (self.args.max_x,self.args.max_y),
+            (self.args.min_x,self.args.min_y),
+            )), crs="EPSG:3413")
+        
+
+
+    def setup_transforms() -> dict[str, CustomTransforms]:
+        # TODO: Convert IABP points to polar grid
+        pass
+
+    def check_dataloader() -> None:
+        # TODO
+        pass
+
     def run_eval(self) -> None:
         """Proceed to evaluation."""
+        dataset_manager = self.setup_dataset_manager()
         dask_cluster = setup_dask(self.args)
-        data_dir1 = self.args.data_dir1
-        data_dir2 = self.args.data_dir2
+        
+        transforms = self.setup_transforms()
 
-        transf_fct = CustomTransforms(
-            transform_name="rename_subset_vars",
-            dict_rename=DICT_RENAME_CMEMS,
-            list_vars=LIST_VARS_GLONET,
-        )
-        self.args.dclogger.info("Creating datasets.")
-        dataset_1 = DCEmptyDataset(
-            conf_args=self.args,
-            root_data_dir= data_dir1,
-            list_dates=self.args.list_start_dates,
-            transform_fct=transf_fct,
-        )
+        dataloader = dataset_manager.get_dataloader(
+            # TODO: set this up
+            pred_alias="PLACEHOLDER",
+            ref_alias="PLACEHOLDER",
+            batch_size=self.args.batch_size,
+            pred_transform=None,
+            ref_transform=None,
+            )
 
-        dataset_2 = DCEmptyDataset(
-            conf_args=self.args,
-            root_data_dir= data_dir2,
-            list_dates=self.args.list_start_dates,
-            transform_fct=transf_fct,
-        )
-        # 1. Chargement des données de référence et des prédictions avec DatasetLoader
-        data_loader = DatasetLoader(
-            pred_dataset=dataset_1,
-            ref_dataset=dataset_2
-        )
+        self.check_dataloader(dataloader)
 
-        # 3. Exécution de l’évaluation sur plusieurs modèles
-        evaluator = Evaluator(
-            self.args,
-            dask_cluster=dask_cluster, metrics=None,
-            data_container={'glonet': data_loader},
-        )
-
-        metrics = [
+        # Forecast accuracy metrics
+        accuracy_metrics = [
             MetricComputer(
-                dc_logger=self.args.dclogger,
-                exc_handler=self.args.exception_handler,
-                metric_name='rmse', plot_result=False,
-            ),
-
-            MetricComputer(
-                dc_logger=self.args.dclogger,
-                exc_handler=self.args.exception_handler,
-                metric_name='energy_cascad',
-                plot_result=False,
-                var="uo", depth=2,
+                metric_name='rmse',
             ),
         ]
-        ''' TODO : check error on oceanbench : why depth = 0 ? -> crash
-            MetricComputer(
-                dc_logger=test_vars.dclogger,
-                exc_handler=test_vars.exception_handler,
-                metric_name='euclid_dist',
-                plot_result=True,
-                minimum_latitude=0,
-                maximum_latitude=10,
-                minimum_longitude=0,
-                maximum_longitude=10,
-            ),'''
 
-        evaluator.set_metrics(metrics)
-        self.args.dclogger.info("Run computation of metrics.")
-        results = evaluator.evaluate()
+        # Physical realism metrics
+        # TODO
 
-        self.args.dclogger.info(f"Computed metrics : {results}")
+        # User-oriented metrics
+        # TODO
+
+        # Computational efficiency metrics
+        # TODO
+
+        accuracy_evaluator = Evaluator(
+            dask_cluster=dask_cluster,
+            metrics=accuracy_metrics,
+            dataloader=dataloader,
+        )
+
+        accuracy_results = accuracy_evaluator.evaluate()
+
+        # Check that results exist
+        assert len(accuracy_results) > 0
+
+        # Check that all expected fields are in results
+        for result in accuracy_results:
+            assert "date" in result
+            assert "metric" in result
+            assert "result" in result
+        logger.info(f"Test Results: {accuracy_results}")
 
